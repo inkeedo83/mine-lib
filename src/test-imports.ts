068fc5efdc42a8659ed3278e438523bdfc0eb899
inkeedo83 <inkeedo83@gmail.com>
2025-03-31 13:51:55 +0300

    fix name

--- src/test-imports.ts
+++ src/test-imports.ts
@@ -1,6 +1,8 @@
 // Тестовый файл для проверки импортов
 import { PrismaModule } from "@mine/prisma";
 import { bootstrapTelemetry } from "@mine/telemetry";
+// Если нужно импортировать что-то из основного модуля
+// import { ... } from '@mine/core';
 
 console.log("PrismaModule:", PrismaModule);
 console.log("bootstrapTelemetry:", bootstrapTelemetry);
